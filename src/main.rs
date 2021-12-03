e75c2e7d824d26f819680e87fc01fe4df6371b1a
Kurt Wolf <kurtwolfbuilds@gmail.com>
2021-12-03 11:45:50 -0500

    fix

--- src/main.rs
+++ src/main.rs
@@ -157,7 +157,8 @@ mod test {
             touch(file).unwrap();
             // tries to eliminate ties between files. 1ms should be more than enough
             // and we dont need a ton of tests for this program where 1ms is noticeable.
-            std::thread::sleep(std::time::Duration::from_millis(1));
+            // apparently 1ms isn't enough for github actions because ...... reasons?
+            std::thread::sleep(std::time::Duration::from_millis(2));
         }
         TempFiles {
             dir: tempdir,
